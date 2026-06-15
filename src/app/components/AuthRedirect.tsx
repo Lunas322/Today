62262dd840db0c136745c23b61576e993b4bb7a7
Lunas322 <fpvmxm333477@gmail.com>
2026-06-15 20:53:49 +0900

    feat: 리디렉션 Provider 추가

---
+++ src/app/components/AuthRedirect.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function AuthRedirect({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = useAuthStore((state) => state.user);
+  const loading = useAuthStore((state) => state.loading);
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+useEffect(() => {
+  if (loading) return;
+
+  const isLoginPage = pathname.startsWith('/login')
+  const isHomePage = pathname.startsWith('/home')
+    if(user && isLoginPage){
+        router.replace('/home')
+    } 
+    if(!user&&isHomePage){
+        router.push('/login')
+    }
+}
+, [user, loading, pathname]);
+
+
+  return <>{children}</>;
+}
