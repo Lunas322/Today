fd7cb74654020748dcd7c87ef0dfb0e3cc390e04
Lunas322 <fpvmxm333477@gmail.com>
2026-06-15 20:54:19 +0900

    feat: 구글 로그인 버튼 UI 및 기능 추가

---
+++ src/app/login/GoogleLoginButton.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import Image from "next/image";
+import { auth } from "../lib/firebase";
+
+
+export default function GoogleLoginButton () {
+
+        const handleLogin = async () =>{
+            const provider = new GoogleAuthProvider()
+        await signInWithPopup(auth, provider);
+        }
+
+    return (
+                  <button className="w-full flex items-center justify-center gap-3 border border-gray-200 rounded-2xl py-3 font-medium hover:bg-gray-50 transition"
+                  onClick={()=>handleLogin()}
+                  >
+                    <Image
+                      src="https://www.svgrepo.com/show/475656/google-color.svg"
+                      alt="google"
+                        width={20}
+                        height={20}
+                    />
+                    Google로 시작하기
+                  </button>
+    )
+}
